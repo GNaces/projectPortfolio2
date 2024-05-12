e7ec89aac7b50762881b6be9fa53fb8e7197522b
Godwin C. Naces <godwinnaces@gmail.com>
2024-05-12 10:04:09 +0000

    Add modal pop up content in HTML, style pop up and code the pop in js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const modal = document.getElementById("myModal");
+
+for (let button of buttons) {
+    button.addEventListener("click", function() {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
+}
+
+/**
+ * Activate class when question mark button clicked
+ */
+function styleModal() {
+    modal.classList.toggle("active");
+}
