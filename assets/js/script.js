743dc0534d79c26ef7a5e2808f6087d4ac5daac1
Godwin C. Naces <godwinnaces@gmail.com>
2024-05-13 10:38:53 +0000

    add js code to populate player choice and select computer choice up clicking the selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 const modal = document.getElementById("myModal");
 const cards = document.getElementById("myCards");
-const buttons = document.getElementsByClassName("select")
+const buttons = document.getElementsByClassName("select");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
+const choices = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
 
 for (let button of buttons) {
@@ -24,4 +25,27 @@ function styleModal() {
  */
 function styleCards() {
     cards.classList.toggle("shoot");
+}
+
+function playGame(playerChoice) {
+
+    //Get a random value for the computer
+    let computerChoice = Math.floor(Math.random() * 13);
+
+    //Getting string results for the player and computers from the integer choice"
+    const computerChoiceString = choices[computerChoice];
+    const playerChoiceString = choices[playerChoice];
+
+    //Taken this code from code institutes portfolio project scope
+    //Set the player image to the players choice
+    playerImage.src = `assets/image/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    //Taken this code from code institutes portfolio project scope
+    //Set the computer image to the computers choice
+    computerImage.src = `assets/image/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+
+
+
 }
